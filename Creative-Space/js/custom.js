2b1eb6d49f17e6cf723ef45d9d137e9e8fed1b8a
Simeon Petrov <petrov.v.simeon@gmail.com>
2016-04-14 00:42:00 +0300

    Responsive site for coworking space

---
+++ Creative-Space/js/custom.js
@@ -0,0 +1,27 @@
+var $ = window.$,
+    input = $(".inputHolder").children("input"),
+    mobileMenuBtn = $(".mobileMenuIcon"),
+    mobileMenu = $(".mobileMenu");
+
+$(document).ready(function () {
+    'use strict';
+
+    
+    $(".inputHolder input, .inputHolder textarea").on("keyup", function () {
+        $(this).prev().hide();
+        
+        if ($(this).val() === "") {
+            window.console.log("Empty Input");
+            $(this).prev().show();
+
+        } else {
+            $(this).prev().hide();
+        }
+    });
+    
+    mobileMenuBtn.on("click", function () {
+        mobileMenu.slideToggle();
+    });
+    
+});
+
